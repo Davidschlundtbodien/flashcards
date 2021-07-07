5c86f677be9dfc0e707c611f9910bdbfc416c8a0
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-07-07 14:35:32 -0600

    Add game class

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,21 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
   constructor() {}
+
+  start() {
+    const cards = []
+    prototypeQuestions.forEach(question => {
+      cards.push(new Card(question.id, question.question, question.answers, question.correctAnswer))
+    })
+    const deck = new Deck(cards)
+    this.currentRound = new Round(deck)
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
